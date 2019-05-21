ae671fbb28e365b793e9207e3802c586a75b9ee1
Gun Gun Priatna <gungunpriatna02@gmail.com>
2019-05-21 17:43:17 +0700

    create project. simple mpv

---
+++ app/src/main/java/com/gungunpriatna/myunittesting/MainModel.java
@@ -0,0 +1,13 @@
+package com.gungunpriatna.myunittesting;
+
+public class MainModel {
+    private double volume;
+
+    public MainModel(double volume) {
+        this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+}
---
+++ app/src/main/java/com/gungunpriatna/myunittesting/MainView.java
@@ -0,0 +1,5 @@
+package com.gungunpriatna.myunittesting;
+
+public interface MainView {
+    void showVolume(MainModel model);
+}
